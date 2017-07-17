cc5d777c7af50ca7845c10ce616a2c220dec22bc
hunglq28389 <hunglq28389@gmail.com>
2017-07-17 14:38:20 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\CapBanSaoTrichLuc;
+use App\chamecon;
 use App\CongDan;
+use App\ConNuoi;
 use App\DmDvQl;
 use App\DnDvLt;
 use App\DnDvLtReg;
 use App\DonViDvVt;
 use App\DonViDvVtReg;
 use App\GeneralConfigs;
+use App\giamho;
+use App\KetHon;
 use App\KhaiSinh;
+use App\KhaiTu;
 use App\Register;
+use App\TTHonNhan;
 use App\Users;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
@@ -30,15 +37,33 @@ class HomeController extends Controller
                 $count = '';
                 if(session('admin')->level == 'T'){
 
-                    $count['slcd'] = CongDan::all()->count();
+                    $count['slkt'] = KhaiTu::all()->count();
                     $count['slks'] = KhaiSinh::all()->count();
+                    $count['sltthn'] = TTHonNhan::all()->count();
+                    $count['slkh'] = KetHon::all()->count();
+                    $count['slcn'] = ConNuoi::all()->count();
+                    $count['slgh'] = giamho::all()->count();
+                    $count['slcmc'] = chamecon::all()->count();
+                    $count['slbstl'] = CapBanSaoTrichLuc::all()->count();
 
                 }elseif(session('admin')->level =='H'){
-                    $count['slcd'] = CongDan::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['slkt'] = KhaiTu::where('mahuyen',session('admin')->mahuyen)->count();
                     $count['slks'] = KhaiSinh::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['sltthn'] = TTHonNhan::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['slkh'] = KetHon::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['slcn'] = ConNuoi::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['slgh'] = giamho::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['slcmc'] = chamecon::where('mahuyen',session('admin')->mahuyen)->count();
+                    $count['slbstl'] = CapBanSaoTrichLuc::where('mahuyen',session('admin')->mahuyen)->count();
                 }else{
-                    $count['slcd'] = CongDan::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['slkt'] = KhaiTu::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
                     $count['slks'] = KhaiSinh::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['sltthn'] = TTHonNhan::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['slkh'] = KetHon::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['slcn'] = ConNuoi::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['slgh'] = giamho::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['slcmc'] = chamecon::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
+                    $count['slbstl'] = CapBanSaoTrichLuc::where('mahuyen',session('admin')->mahuyen)->where('maxa',session('admin')->maxa)->count();
                 }
                 return view('dashboard')
                     ->with('count',$count)
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,6 +19,23 @@
 
     <div class="row">
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="dashboard-stat blue-madison">
+                <div class="visual">
+                    <i class="fa fa-building"></i>
+                </div>
+                <div class="details">
+                    <div class="number"></div>
+                    <div class="desc">
+                        Thông tin khai sinh<br>
+                        <h5>{{$count['slks']}} Hồ sơ</h5>
+                    </div>
+                </div>
+                <a class="more" href="{{url('khaisinh')}}">
+                    Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
             <div class="dashboard-stat red-intense">
                 <div class="visual">
                     <i class="fa fa-building"></i>
@@ -26,28 +43,113 @@
                 <div class="details">
                     <div class="number"></div>
                     <div class="desc">
-                        Thông tin công dân<br>
-                        <h5>{{$count['slcd']}} công dân</h5>
+                        Thông tin khai tử<br>
+                        <h5>{{$count['slkt']}} Hồ sơ</h5>
                     </div>
                 </div>
-                <a class="more" href="{{url('congdan')}}">
+                <a class="more" href="{{url('khaitu')}}">
                     Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
                 </a>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <div class="dashboard-stat blue-madison">
+            <div class="dashboard-stat red-pink">
                 <div class="visual">
-                    <i class="fa fa-car"></i>
+                    <i class="fa fa-building"></i>
                 </div>
                 <div class="details">
                     <div class="number"></div>
                     <div class="desc">
-                        Thông tin khai sinh<br>
-                        <h5>{{$count['slks']}} khai sinh</h5>
+                        Tình trạng hôn nhân<br>
+                        <h5>{{$count['sltthn']}} Hồ sơ</h5>
                     </div>
                 </div>
-                <a class="more" href="{{url('khaisinh')}}">
+                <a class="more" href="{{url('tthonnhan')}}">
+                    Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="dashboard-stat yellow-gold">
+                <div class="visual">
+                    <i class="fa fa-building"></i>
+                </div>
+                <div class="details">
+                    <div class="number"></div>
+                    <div class="desc">
+                        Thông tin kết hôn<br>
+                        <h5>{{$count['slkh']}} Hồ sơ</h5>
+                    </div>
+                </div>
+                <a class="more" href="{{url('kethon')}}">
+                    Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="dashboard-stat blue-chambray">
+                <div class="visual">
+                    <i class="fa fa-building"></i>
+                </div>
+                <div class="details">
+                    <div class="number"></div>
+                    <div class="desc">
+                        Đăng ký con nuôi<br>
+                        <h5>{{$count['slcn']}} Hồ sơ</h5>
+                    </div>
+                </div>
+                <a class="more" href="{{url('dangkyconnuoi')}}">
+                    Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="dashboard-stat yellow">
+                <div class="visual">
+                    <i class="fa fa-building"></i>
+                </div>
+                <div class="details">
+                    <div class="number"></div>
+                    <div class="desc">
+                        Đăng ký giám hộ<br>
+                        <h5>{{$count['slgh']}} Hồ sơ</h5>
+                    </div>
+                </div>
+                <a class="more" href="{{url('dangkygiamho')}}">
+                    Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="dashboard-stat purple-plum">
+                <div class="visual">
+                    <i class="fa fa-building"></i>
+                </div>
+                <div class="details">
+                    <div class="number"></div>
+                    <div class="desc">
+                        Đăng ký nhận cha mẹ con<br>
+                        <h5>{{$count['slcmc']}} Hồ sơ</h5>
+                    </div>
+                </div>
+                <a class="more" href="{{url('dangkynhanchamecon')}}">
+                    Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="dashboard-stat yellow-casablanca">
+                <div class="visual">
+                    <i class="fa fa-building"></i>
+                </div>
+                <div class="details">
+                    <div class="number"></div>
+                    <div class="desc">
+                        Cấp trích lục bản sao<br>
+                        <h5>{{$count['slbstl']}} Hồ sơ</h5>
+                    </div>
+                </div>
+                <a class="more" href="{{url('capbansaotrichluc')}}">
                     Xem chi tiết <i class="m-icon-swapright m-icon-white"></i>
                 </a>
             </div>
